62e84d3156ada7351f5d54252b3974f1ce61ae54
hmar321 <hmauricio.ar@gmail.com>
2024-04-29 11:46:19 +0200

    Add project files.

---
+++ MvcCoreDeployment/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreDeployment.Models;
+using System.Diagnostics;
+
+namespace MvcCoreDeployment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["SALUDO"] = "Nuestra app Deployment de lunes";
+            Random random = new Random();
+            List<int> numeros = new List<int>();
+            for (int i = 1; i <=10; i++)
+            {
+                numeros.Add(random.Next(1,50));
+            }
+            return View(numeros);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
